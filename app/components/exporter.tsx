85019990a1e9851d6d07579d5ab7b8431d587a5c
Christia Hand <9104690@qq.com>
2023-09-19 13:34:51 +0800

    Update exporter.tsx

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -427,14 +427,14 @@ export function ImagePreviewer(props: {
         ref={previewRef}
       >
         <div className={styles["chat-info"]}>
-          <div className={styles["logo"] + " no-dark"}>
+          {/* <div className={styles["logo"] + " no-dark"}>
             <NextImage
               src={ChatGptIcon.src}
               alt="logo"
               width={50}
               height={50}
             />
-          </div>
+          </div> */}
 
           <div>
             <div className={styles["main-title"]}>ChatGPT Next Web</div>
